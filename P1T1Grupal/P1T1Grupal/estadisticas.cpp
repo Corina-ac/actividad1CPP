a02c2cff45d49c96b28a8d7afeb7eee03d6f35c9
Corina-ac <caacosta7@espe.edu.ec>
2025-04-29 22:01:03 -0500

    Update estadisticas.cpp

--- P1T1Grupal/P1T1Grupal/estadisticas.cpp
+++ P1T1Grupal/P1T1Grupal/estadisticas.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <vector>
 #include "utils.h"
 using namespace std;
-void Estadisticas(){
-cout<<"Funcion no implementada aun"<<endl;
+
+void Estadisticas() {
+    int n = readInt("¿Cuántos números desea ingresar? ");
+    vector<double> nums(n);
+    
+    for (int i = 0; i < n; i++) {
+        nums[i] = readDouble("Ingrese número " + to_string(i+1) + ": ");
+    }
+    
+    double max = nums[0], min = nums[0], sum = 0;
+    for (double num : nums) {
+        if (num > max) max = num;
+        if (num < min) min = num;
+        sum += num;
+    }
+    
+    cout << "Máximo: " << max << endl;
+    cout << "Mínimo: " << min << endl;
+    cout << "Promedio: " << sum/n << endl;
 }
